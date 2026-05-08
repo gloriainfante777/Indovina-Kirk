4e4e1dbbb0e77a73d2d9828695e2aa56cb703228
ElisaScuola <elisa.quartieri14@gmail.com>
2026-05-08 09:56:20 +0200

    aggiunta cg

--- src/CharacterGrid.java
+++ src/CharacterGrid.java
@@ -1,3 +1,13 @@
+package ui;
+import characters.CharacterCard;
+import java.util.List;
+
 public class CharacterGrid {
-    private final
+    private final  int cellW = 140;   // 5:7 — aumentabili
+    private final int cellH = 196;
+
+    private final List<CharacterCard> characters;
+
+    public CharacterCard selectedCharacter = null;
+
 }
