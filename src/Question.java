8fba5b178cda27daaa60aaa1db913e42b3486dda
annanicoli <annaprogrammacisco@gmail.com>
2026-05-14 18:14:23 +0200

    Question

---
+++ src/Question.java
@@ -0,0 +1,29 @@
+import characters.Tratti;
+
+
+package manager;
+
+    public class Question {
+
+        private final String testo;
+        private final Tratti trattiAssociato;
+
+        public Question(String testo, Tratti trattiAssociato) {
+            this.testo = testo;
+            this.trattiAssociato = trattiAssociato;
+        }
+
+        public String getTesto() {
+            return testo;
+        }
+
+        public Tratti getTrattiAssociato() {
+            return trattiAssociato;
+        }
+
+        @Override
+        public String toString() {
+            return testo;
+        }
+    }
+}
