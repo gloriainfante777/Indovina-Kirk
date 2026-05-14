bea887e77c1be3fe695d4013ae00ce2017e69c6f
ElisaScuola <elisa.quartieri14@gmail.com>
2026-05-14 17:33:09 +0200

    d

--- src/Domande.java
+++ src/Domande.java
@@ -64,6 +64,4 @@ public class Domande {
         }
         return turn;
     }
-
-
 }
---
+++ src/characters/Tratti.java
@@ -0,0 +1,38 @@
+package characters;
+public enum Tratti {
+        CAPELLI_SCURI,
+        CAPELLI_BIONDI,
+        CAPELLI_BIANCHI,
+        CAPELLI_GRIGI,
+        CAPELLI_ROSSI,
+
+        CAPELLI_CORTI,
+        CAPELLI_LUNGHI,
+
+        OCCHI_SCURI,
+        OCCHI_AZZURRI,
+        OCCHI_MARRONI,
+
+        CARNAGIONE_BIANCA,
+        CARNAGIONE_SCURA,
+
+        PELATO,
+        BAFFI,
+        PIZZETTO,
+        OCCHIALI,
+        OCCHIALI_DA_SOLE,
+
+        VECCHIO,
+        GIOVANE,
+
+        NASO_GRANDE,
+        LENTIGGINI,
+        MUSCOLOSO,
+        PAFFUTO,
+
+        FERITA_COLLO,
+        FRONTE_LARGA,
+        FACCIA_STORTA,
+        TRIPLO_MENTO
+    }
+
